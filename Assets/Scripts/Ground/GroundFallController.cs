fc401887c485c546f198d86d63463aee724e1947
Mohadese Nikoogoftar <nikomohdse@gmail.com>
2024-12-04 17:51:07 +0330

    first commit

---
+++ Assets/Scripts/Ground/GroundFallController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundFallController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+    public IEnumerator SetRigidBodyValue(){
+        yield return new WaitForSeconds(0.8f);
+        rb.isKinematic = false;
+        rb.useGravity = true;
+    }
+}
